bfbdb0a9e175ca947ba837668585cf1ae1f1ec04
Nikhil Tiwary <ntiwary61@gmail.com>
2023-10-03 23:39:30 +0530

    feat: add header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,29 @@
 import "./Header.css";
+import { Autocomplete } from "@react-google-maps/api";
+import { Search } from "@mui/icons-material";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@mui/material";
 
 export default function Header() {
   return (
-    <>
-      <div>Header</div>
-    </>
+    <AppBar position="static">
+      <Toolbar className="toolbar">
+        <Typography variant="h5" className="title">
+          Travel Advisor
+        </Typography>
+        <Box display="flex" className="box">
+          <Typography variant="h6" className="title">
+            Explore New Places
+          </Typography>
+          {/* <Autocomplete> */}
+          <div className="search">
+            <div className="SearchIcon">
+              <Search />
+            </div>
+            <InputBase placeholder="Search..." className="Root Input" />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 }
